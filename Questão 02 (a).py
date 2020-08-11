f5c1155f78871fe325ff0df2908783d6106dae42
Matheus Duarte <62728642+matheusduarte96@users.noreply.github.com>
2020-08-11 12:47:18 -0300

    Delete Questão 02 (a).py

--- Questão 02 (a).py
+++
@@ -1,9 +0,0 @@
-#Questão 02 - (a)
-
-#Solicita as informações de dia, mês e ano para o usuário
-dia = int(input("Digite o número de um dia: "))
-mes = int(input("Digite o número de um mês: "))
-ano = int(input("Digite o número de um ano: "))
-
-#Imprimi(mostra) ao usuário o formato da data a partir das informações dadas.
-print("A data informada é {}/{:02d}/{}".format(dia, mes, ano))
