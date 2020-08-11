a1dcd78c7c1e3e4ee42c5752e52e6b2e3e6309fa
Matheus Duarte <62728642+matheusduarte96@users.noreply.github.com>
2020-08-11 12:43:41 -0300

    Add files via upload

---
+++ Questão 02 (c).py
@@ -0,0 +1,10 @@
+#Questão 02 - (c)
+
+#Solicita ao usuário um número inteiro.
+num = int(input("Digite um número: "))
+
+#Calcula o cubo do número informado.
+cubo = num**3
+
+#Imprimi(mostra) ao usuário o cubo do número informado.
+print(f'O cubo do número digitado é {cubo}')
