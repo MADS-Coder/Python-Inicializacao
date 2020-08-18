d0cfc75c64abcbf32737cc2557b91bb818c29d2a
Matheus Duarte <mtduarteadm@hotmail.com>
2020-08-18 18:42:35 -0300

    Respostas da Atividade Remota - Semana 02 - Tarefa 1 (Respostas)
    Respostas da Atividade Remota - Semana 02 - Tarefa 1 (Respostas)

---
+++ Respostas da Atividade Remota - Semana 02 - Tarefa 1/Questão 01.py
@@ -0,0 +1,10 @@
+#Questão 01
+
+#Solicita ao usuário um nome.
+nome = str(input("Escreva um nome: "))
+
+#Retorna ao comprimento do objeto.
+carac = len(nome)
+
+#Imprimi (mostra) ao usuário o comprimento de itens (caracteres) do nome informado.
+print(f'O nome {nome} possui {carac} caracteres.')
