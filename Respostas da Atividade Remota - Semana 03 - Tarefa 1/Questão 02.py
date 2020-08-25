7eae913c2258e710c467c44a437a030ecbf4bab4
Matheus Duarte <mtduarteadm@hotmail.com>
2020-08-25 14:18:34 -0300

    Respostas da Atividade Remota - Semana 03 - Tarefa 1
    Respostas da Atividade Remota - Semana 03 - Tarefa 1 (Respostas)

---
+++ Respostas da Atividade Remota - Semana 03 - Tarefa 1/Questão 02.py
@@ -0,0 +1,9 @@
+#Questão 02
+
+numero = int(input("Escreva um número: "))
+
+if numero % 2 == 0:
+    print(f"O número {numero} é par, portanto é FALSO")
+
+else:
+    print(f"O número {numero} é ímpar, portanto é VERDADEIRO")
